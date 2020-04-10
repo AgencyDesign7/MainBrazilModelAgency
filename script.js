b042d6d1718c0f80d04bf9a2e7ac914b9f0d5b06
adeniltonxavier14@hotmail.com.br <adeniltonxavier14@hotmail.com>
2020-04-10 08:14:15 -0300

    insert content categorys

--- script.js
+++ script.js
@@ -1,158 +1,300 @@
-window.onload = function () {
-    navBar()
-    var pageName = location.pathname.split('/').pop()
-    switch (pageName) {
-        case "index.html":
-            slideTitle();
-            break;
+window.onload = function() {
+  navBar()
+  var pageName = location.pathname.split('/').pop()
+  switch (pageName) {
+    case "index.html":
+      slideTitle();
+      break;
 
-        case "shows.html":
-            AudioControls();
-            break;
-        default:
-            break;
-    }
+    case "shows.html":
+      AudioControls();
+      break;
+    default:
+      break;
+  }
 }
 
 function navBar() {
-    var navButton = document.querySelector('.menu-button');
-    var iconMenuClick = document.querySelector('.icon-2')
-    var navMenu = document.querySelector('.nav-menu')
-    var spanInset = document.querySelector('.w-nav-overlay')
-    var navbarDiv = document.querySelector("[data-wf-ignore]")
+  var navButton = document.querySelector('.menu-button');
+  var iconMenuClick = document.querySelector('.icon-2')
+  var navMenu = document.querySelector('.nav-menu')
+  var spanInset = document.querySelector('.w-nav-overlay')
+  var navbarDiv = document.querySelector("[data-wf-ignore]")
 
-    iconMenuClick.addEventListener('click', () => {
+  iconMenuClick.addEventListener('click', () => {
 
-        if (navButton.classList[2] == 'w--open') {
-            navButton.classList.remove('w--open')
-            navMenu.innerHTML = `<a href="./index.html" class="nav-link-2 w-nav-link">HOME</a><a
+    if (navButton.classList[2] == 'w--open') {
+      navButton.classList.remove('w--open')
+      navMenu.innerHTML = `<a href="./index.html" class="nav-link-2 w-nav-link">HOME</a><a
         href="./composite.html" class="nav-link-3 w-nav-link">COMPOSITE</a><a href="./book.html"
         class="nav-link-4 w-nav-link">BOOK</a><a href="./parceiros.html" class="nav-link-5 w-nav-link">PARCEIROS</a><a
         href="./contato.html" class="nav-link-6 w-nav-link">CONTATO</a>`
-            spanInset.innerHTML = '';
-            navbarDiv.classList.remove('an-nav-bf')
-            iconMenuClick.classList.remove('w-menu-open-nav')
-        } else {
-            navMenu.innerHTML = '';
-            spanInset.innerHTML = `
+      spanInset.innerHTML = '';
+      navbarDiv.classList.remove('an-nav-bf')
+      iconMenuClick.classList.remove('w-menu-open-nav')
+    } else {
+      navMenu.innerHTML = '';
+      spanInset.innerHTML = `
         <nav role="navigation" class="nav-menu w-nav-menu mob-nav" style="transform: translateY(0px) translateX(0px); transition: transform 400ms ease 0s;" data-nav-menu-open="">
         <a href="./index.html" class="nav-link-2 w-nav-link w--nav-link-open" style="">HOME</a>
         <a href="./sobrenos.html" class="nav-link-3 w-nav-link w--nav-link-open" style="">SOBRE NÓS</a>
         <a href="#" class="nav-link-4 w-nav-link w--nav-link-open" style="" onClick = showCategoryModels() >MODELOS</a>
         <div class="sub-category-models style-sub-category">
         <a href="./curos.html" class="nav-link-5 w-nav-link w--nav-link-open" style="">BABY</a>
+        <div class="style-sub-category">
+              <div class="sub-baby">
+                <div class="sub-imgs">
+                  <img href="" >
+                  <img href="">
+                </div>
+                <div class="sub-texts">
+                  <p>MAN</p>
+                  <p>WOMAN</p>
+                </div>
+              </div>
+         </div>
         <a href="./curos.html" class="nav-link-5 w-nav-link w--nav-link-open" style="">KID'S</a>
+        <div class="style-sub-category">
+                      <div class="sub-baby">
+                        <div class="sub-imgs">
+                          <img href="" >
+                          <img href="">
+                        </div>
+                        <div class="sub-texts">
+                          <p>MAN</p>
+                          <p>WOMAN</p>
+                        </div>
+                      </div>
+                 </div>
         <a href="./curos.html" class="nav-link-5 w-nav-link w--nav-link-open" style="">PRÉ-TEEN</a>
+        <div class="style-sub-category">
+                      <div class="sub-baby">
+                        <div class="sub-imgs">
+                          <img href="" >
+                          <img href="">
+                        </div>
+                        <div class="sub-texts">
+                          <p>MAN</p>
+                          <p>WOMAN</p>
+                        </div>
+                      </div>
+                 </div>
         <a href="./curos.html" class="nav-link-5 w-nav-link w--nav-link-open" style="">MORE AGE</a>
+        <div class="style-sub-category">
+                      <div class="sub-baby">
+                        <div class="sub-imgs">
+                          <img href="" >
+                          <img href="">
+                        </div>
+                        <div class="sub-texts">
+                          <p>MAN</p>
+                          <p>WOMAN</p>
+                        </div>
+                      </div>
+                 </div>
         <a href="./curos.html" class="nav-link-5 w-nav-link w--nav-link-open" style="">PLUS SIZE</a>
+        <div class="style-sub-category">
+                      <div class="sub-baby">
+                        <div class="sub-imgs">
+                          <img href="" >
+                          <img href="">
+                        </div>
+                        <div class="sub-texts">
+                          <p>MAN</p>
+                          <p>WOMAN</p>
+                        </div>
+                      </div>
+                 </div>
         <a href="./curos.html" class="nav-link-5 w-nav-link w--nav-link-open" style="">COMERCIAL</a>
+        <div class="style-sub-category">
+                      <div class="sub-baby">
+                        <div class="sub-imgs">
+                          <img href="" >
+                          <img href="">
+                        </div>
+                        <div class="sub-texts">
+                          <p>MAN</p>
+                          <p>WOMAN</p>
+                        </div>
+                      </div>
+                 </div>
         <a href="./curos.html" class="nav-link-5 w-nav-link w--nav-link-open" style="">FASHION</a>
+        <div class="style-sub-category">
+                      <div class="sub-baby">
+                        <div class="sub-imgs">
+                          <img href="" >
+                          <img href="">
+                        </div>
+                        <div class="sub-texts">
+                          <p>MAN</p>
+                          <p>WOMAN</p>
+                        </div>
+                      </div>
+                 </div>
         <a href="./curos.html" class="nav-link-5 w-nav-link w--nav-link-open" style="">FITNESS</a>
+        <div class="style-sub-category">
+                      <div class="sub-baby">
+                        <div class="sub-imgs">
+                          <img href="" >
+                          <img href="">
+                        </div>
+                        <div class="sub-texts">
+                          <p>MAN</p>
+                          <p>WOMAN</p>
+                        </div>
+                      </div>
+                 </div>
         <a href="./curos.html" class="nav-link-5 w-nav-link w--nav-link-open" style="">ANDROGYNOUS</a>
+        <div class="style-sub-category">
+                      <div class="sub-baby">
+                        <div class="sub-imgs">
+                          <img href="" >
+                          <img href="">
+                        </div>
+                        <div class="sub-texts">
+                          <p>MAN</p>
+                          <p>WOMAN</p>
+                        </div>
+                      </div>
+                 </div>
         <a href="./curos.html" class="nav-link-5 w-nav-link w--nav-link-open" style="">URBAN</a>
+        <div class="style-sub-category">
+                      <div class="sub-baby">
+                        <div class="sub-imgs">
+                          <img href="" >
+                          <img href="">
+                        </div>
+                        <div class="sub-texts">
+                          <p>MAN</p>
+                          <p>WOMAN</p>
+                        </div>
+                      </div>
+                 </div>
         </div>
         </a>
         <a href="./curos.html" class="nav-link-5 w-nav-link w--nav-link-open" style="">CURSOS</a>
+        <div class="style-sub-category">
+          <a href="./curos.html" class="nav-link-5 w-nav-link w--nav-link-open" style="">EM BREVE</a>
+         </div>
         <a href="./revista.html" class="nav-link-6 w-nav-link w--nav-link-open" style="">REVISTA</a>
+         <div class="style-sub-category">
+           <a href="./curos.html" class="nav-link-5 w-nav-link w--nav-link-open" style="">EM BREVE</a>
+        </div>
         <a href="./eventos.html" class="nav-link-6 w-nav-link w--nav-link-open" style="">EVENTOS</a>
         <a href="./artista.html" class="nav-link-6 w-nav-link w--nav-link-open" style="">ARTISTAS</a>
+        <div class="style-sub-category">
+                <a href="./curos.html" class="nav-link-5 w-nav-link w--nav-link-open" style="">BABY</a>
+                <a href="./curos.html" class="nav-link-5 w-nav-link w--nav-link-open" style="">KID'S</a>
+                <a href="./curos.html" class="nav-link-5 w-nav-link w--nav-link-open" style="">PRÉ-TEEN</a>
+                <a href="./curos.html" class="nav-link-5 w-nav-link w--nav-link-open" style="">MORE AGE</a>
+                <a href="./curos.html" class="nav-link-5 w-nav-link w--nav-link-open" style="">PLUS SIZE</a>
+                <a href="./curos.html" class="nav-link-5 w-nav-link w--nav-link-open" style="">COMERCIAL</a>
+                <a href="./curos.html" class="nav-link-5 w-nav-link w--nav-link-open" style="">FASHION</a>
+                <a href="./curos.html" class="nav-link-5 w-nav-link w--nav-link-open" style="">FITNESS</a>
+                <a href="./curos.html" class="nav-link-5 w-nav-link w--nav-link-open" style="">ANDROGYNOUS</a>
+                <a href="./curos.html" class="nav-link-5 w-nav-link w--nav-link-open" style="">URBAN</a>
+                </div>
         <a href="./parceiros.html" class="nav-link-2 w-nav-link w--nav-link-open" style="">PARCEIROS</a>
         <a href="./facaparte.html" class="nav-link-2 w-nav-link w--nav-link-open" style="">FAÇA PARTE</a>
         <a href="./contatos.html" class="nav-link-2 w-nav-link w--nav-link-open" style="">CONTATOS</a>
         </nav>
     </div>`;
-            navButton.classList.add('w--open')
-            navbarDiv.classList.add('an-nav-bf')
-            iconMenuClick.classList.add('w-menu-open-nav')
+      navButton.classList.add('w--open')
+      navbarDiv.classList.add('an-nav-bf')
+      iconMenuClick.classList.add('w-menu-open-nav')
 
 
-        }
+    }
 
+  })
+
+
+  var navMenuLink = document.querySelectorAll('.w-nav-menu a');
+
+  navMenuLink.forEach(element => {
+    element.addEventListener('mouseenter', event => {
+      event.target.style.backgroundColor = 'white';
+      event.target.style.color = "black"
     })
-
-
-    var navMenuLink = document.querySelectorAll('.w-nav-menu a');
-
-    navMenuLink.forEach(element => {
-        element.addEventListener('mouseenter', event => {
-            event.target.style.backgroundColor = 'white';
-            event.target.style.color = "black"
-        })
-        element.addEventListener('mouseout', event => {
-            event.target.style.backgroundColor = '';
-            event.target.style.color = "white"
-        })
+    element.addEventListener('mouseout', event => {
+      event.target.style.backgroundColor = '';
+      event.target.style.color = "white"
     })
+  })
 }
 
 
 function AudioControls() {
-    var audio1 = document.querySelector('.audio1');
-    var audio2 = document.querySelector('.audio2');
-    var image1 = document.querySelector('.show1')
-    var image2 = document.querySelector('.show2')
-    var iconAudioControl1 = document.querySelector('.iconplay1')
-    var iconAudioControl2 = document.querySelector('.iconplay2')
-    var isplay1 = false;
-    var isplay2 = false;
+  var audio1 = document.querySelector('.audio1');
+  var audio2 = document.querySelector('.audio2');
+  var image1 = document.querySelector('.show1')
+  var image2 = document.querySelector('.show2')
+  var iconAudioControl1 = document.querySelector('.iconplay1')
+  var iconAudioControl2 = document.querySelector('.iconplay2')
+  var isplay1 = false;
+  var isplay2 = false;
 
-    audio1.src = "./media/shows/show1audio.mpeg";
-    audio2.src = "./media/shows/show2audio.mpeg";
-
-
-    function playAudio1() {
-        audio1.play();
-        iconAudioControl1.src = "./media/shows/pauseIcon.png"
-        isplay1 = true;
-    }
-    function pauseAudio1() {
-        audio1.pause();
-        isplay1 = false;
-        iconAudioControl1.src = "./media/shows/playIcon.png"
-    }
-    function playAudio2() {
-        audio2.play();
-        iconAudioControl2.src = "./media/shows/pauseIcon.png"
-        isplay2 = true;
-    }
-    function pauseAudio2() {
-        audio2.pause()
-        isplay2 = false;
-        iconAudioControl2.src = "./media/shows/playIcon.png"
-    }
-
-    iconAudioControl1.addEventListener('click', () => {
-
-        if (!isplay1) {
-            playAudio1();
-            pauseAudio2();
-        } else { pauseAudio1() }
-
-    })
-
-    iconAudioControl2.addEventListener('click', () => {
-        if (!isplay2) {
-            playAudio2()
-            pauseAudio1()
-        } else { pauseAudio2() }
+  audio1.src = "./media/shows/show1audio.mpeg";
+  audio2.src = "./media/shows/show2audio.mpeg";
 
 
+  function playAudio1() {
+    audio1.play();
+    iconAudioControl1.src = "./media/shows/pauseIcon.png"
+    isplay1 = true;
+  }
 
-    })
+  function pauseAudio1() {
+    audio1.pause();
+    isplay1 = false;
+    iconAudioControl1.src = "./media/shows/playIcon.png"
+  }
+
+  function playAudio2() {
+    audio2.play();
+    iconAudioControl2.src = "./media/shows/pauseIcon.png"
+    isplay2 = true;
+  }
+
+  function pauseAudio2() {
+    audio2.pause()
+    isplay2 = false;
+    iconAudioControl2.src = "./media/shows/playIcon.png"
+  }
+
+  iconAudioControl1.addEventListener('click', () => {
+
+    if (!isplay1) {
+      playAudio1();
+      pauseAudio2();
+    } else { pauseAudio1() }
+
+  })
+
+  iconAudioControl2.addEventListener('click', () => {
+    if (!isplay2) {
+      playAudio2()
+      pauseAudio1()
+    } else { pauseAudio2() }
+
+
+
+  })
 }
 
 function slideTitle() {
-    var index = 0;
-    nextSlide()
-    function nextSlide() {
-        var divImgs = document.querySelectorAll('.w-slide');
+  var index = 0;
+  nextSlide()
 
-        divImgs.forEach(element => element.style.display = "none")
-        index++;
-        if (index > divImgs.length) { index = 1 }
-        divImgs[index - 1].style.display = "block";
-        setTimeout(nextSlide, 7000)
-    }
+  function nextSlide() {
+    var divImgs = document.querySelectorAll('.w-slide');
+
+    divImgs.forEach(element => element.style.display = "none")
+    index++;
+    if (index > divImgs.length) { index = 1 }
+    divImgs[index - 1].style.display = "block";
+    setTimeout(nextSlide, 7000)
+  }
 
 
 
@@ -160,7 +302,7 @@ function slideTitle() {
 }
 
 function showCategoryModels() {
-    var subCategoryModels = document.querySelector('.sub-category-models').classList.toggle('showContentBlock');
+  var subCategoryModels = document.querySelector('.sub-category-models').classList.toggle('showContentBlock');
 }
 
 /*
@@ -182,4 +324,3 @@ function searchAndShowSlide() {
     })
 }
 */
-
